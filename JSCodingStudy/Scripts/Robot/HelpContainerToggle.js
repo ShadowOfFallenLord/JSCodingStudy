d49f1661bb70bc32443e6e8477d307b64dea28e9
DESKTOP-E984K83\DarkLord <dfhrhfan12@yandex.ru>
2021-02-06 14:41:44 +0400

    Move open/close help container functional to HelpContainerToggle.js

---
+++ JSCodingStudy/Scripts/Robot/HelpContainerToggle.js
@@ -0,0 +1,21 @@
+﻿var HelpContainer = $('#help_container');
+var doc = $('html');
+
+$('#close_help_button').click(function () {
+    HelpContainer.hide();
+});
+
+html_elements.Buttons.HelpButton.click(function () {
+    HelpContainer.show();
+
+    var screen_width = doc.width();
+    var screen_heigth = doc.height();
+
+    var element_width = HelpContainer.width();
+    var element_heigth = HelpContainer.height();
+
+    HelpContainer.css({
+        left: ((screen_width / 2) - (element_width / 2)),
+        top: ((screen_heigth / 2) - (element_heigth / 2)),
+    });
+});
