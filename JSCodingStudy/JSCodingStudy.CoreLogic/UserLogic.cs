8f5426e490829381273c0e322032e38e625e59c3
DESKTOP-E984K83\DarkLord <dfhrhfan12@yandex.ru>
2021-02-07 23:34:56 +0400

    Add UserLogic

---
+++ JSCodingStudy/JSCodingStudy.CoreLogic/UserLogic.cs
@@ -0,0 +1,46 @@
+﻿using JSCodingStudy.DALInterfaces;
+using JSCodingStudy.LogicInterfaces;
+using JSCodingStudy.UserEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSCodingStudy.CoreLogic
+{
+    public class UserLogic : IUserLogic
+    {
+        private IUserDao dao;
+
+        public UserLogic(IUserDao dao)
+        {
+            this.dao = dao;
+        }
+
+        public bool Add(User user)
+        {
+            return dao.Add(user);
+        }
+
+        public User GetById(int id)
+        {
+            return dao.GetById(id);
+        }
+
+        public User GetByLogin(string login)
+        {
+            return dao.GetByLogin(login);
+        }
+
+        public bool RemoveById(int id)
+        {
+            return dao.RemoveById(id);
+        }
+
+        public bool Update(User user)
+        {
+            return dao.Update(user);
+        }
+    }
+}
