7d0d589b447be73f97285a0dcff67d0cc4e2e421
DESKTOP-E984K83\DarkLord <dfhrhfan12@yandex.ru>
2021-02-07 19:07:37 +0400

    Add Common project

---
+++ JSCodingStudy/JSCodingStudy.Common/ILesson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSCodingStudy.Common
+{
+    public interface ILesson
+    {
+        int Id { get; }
+        string Title { get; }
+    }
+}
