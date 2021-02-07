7da37c9472815a719cb8188371daaf00d8dd1339
DESKTOP-E984K83\DarkLord <dfhrhfan12@yandex.ru>
2021-02-07 18:52:10 +0400

    Add few DALInterfaces

---
+++ JSCodingStudy/JSCodingStudy.DALInterfaces/IUserCodeDao.cs
@@ -0,0 +1,17 @@
+﻿using JSCodingStudy.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSCodingStudy.DALInterfaces
+{
+    public interface IUserCodeDao
+    {
+        bool Add(UserCode code);
+        UserCode GetById(int user_id, int lesson_id);
+        bool Update(UserCode code);
+        bool RemoveById(int user_id, int lesson_id);
+    }
+}
---
+++ JSCodingStudy/JSCodingStudy.DALInterfaces/IUserDao.cs
@@ -0,0 +1,18 @@
+﻿using JSCodingStudy.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSCodingStudy.DALInterfaces
+{
+    public interface IUserDao
+    {
+        bool Add(User user);
+        User GetById(int id);
+        User GetByLogin(string login);
+        bool Update(User user);
+        bool RemoveById(int id);
+    }
+}
