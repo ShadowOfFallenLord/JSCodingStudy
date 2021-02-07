5714d6f5ac28e9c16119329867c09436b3cf8224
DESKTOP-E984K83\DarkLord <dfhrhfan12@yandex.ru>
2021-02-08 00:05:12 +0400

    Add UserCodeLogic

---
+++ JSCodingStudy/JSCodingStudy.CoreLogic/UserCodeLogic.cs
@@ -0,0 +1,35 @@
+﻿using JSCodingStudy.DALInterfaces;
+using JSCodingStudy.LessonsEntities;
+using JSCodingStudy.LogicInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSCodingStudy.CoreLogic
+{
+    public class UserCodeLogic<LessonType> : IUserCodeLogic<LessonType> where LessonType : ILesson
+    {
+        private IUserCodeDao<LessonType> dao;
+
+        public UserCodeLogic(IUserCodeDao<LessonType> dao)
+        {
+            this.dao = dao;
+        }
+
+        public string Get(int user_id, int lesson_id)
+        {
+            return dao.GetById(user_id, lesson_id);
+        }
+
+        public bool Set(int user_id, int lesson_id, string code)
+        {
+            if(!dao.Add(user_id, lesson_id, code))
+            {
+                dao.Update(user_id, lesson_id, code);
+            }
+            return true;
+        }
+    }
+}
