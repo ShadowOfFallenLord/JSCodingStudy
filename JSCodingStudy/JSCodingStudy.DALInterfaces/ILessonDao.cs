b8ca1eef2549945b2bb89e62feff427267c2434e
DESKTOP-E984K83\DarkLord <dfhrhfan12@yandex.ru>
2021-02-07 20:37:29 +0400

    Add ILessonDao

---
+++ JSCodingStudy/JSCodingStudy.DALInterfaces/ILessonDao.cs
@@ -0,0 +1,18 @@
+﻿using JSCodingStudy.LessonsEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSCodingStudy.DALInterfaces
+{
+    public interface ILessonDao<LessonType> : ILesson
+    {
+        bool Add(LessonType user);
+        LessonType GetById(int id);
+        IEnumerable<LessonType> GetAll();
+        bool Update(LessonType user);
+        bool RemoveById(int id);
+    }
+}
