2e7589af10de164aca83a8d2182ca6dc0dbf40cc
DESKTOP-E984K83\DarkLord <dfhrhfan12@yandex.ru>
2021-02-07 18:57:08 +0400

    Move User classes to UserEntities

--- JSCodingStudy/JSCodingStudy.UserEntities/UserLastLessons.cs
+++ JSCodingStudy/JSCodingStudy.UserEntities/UserLastLessons.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace JSCodingStudy.Entities
+namespace JSCodingStudy.UserEntities
 {
     public class UserLastLessons
     {
+        public int UserId { get; set; }
+        public int LessonId { get; set; }
         public int Robot { get; set; }
     }
 }
