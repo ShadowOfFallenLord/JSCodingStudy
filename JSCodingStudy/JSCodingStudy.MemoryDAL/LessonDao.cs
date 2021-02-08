7354a29b5bfa4636a93dfb9562861eec8b593a6b
DESKTOP-E984K83\DarkLord <dfhrhfan12@yandex.ru>
2021-02-08 10:46:28 +0400

    Add generic LessonDao

---
+++ JSCodingStudy/JSCodingStudy.MemoryDAL/LessonDao.cs
@@ -0,0 +1,62 @@
+﻿using JSCodingStudy.DALInterfaces;
+using JSCodingStudy.LessonsEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSCodingStudy.MemoryDAL
+{
+    public class LessonDao<LessonType> : ILessonDao<LessonType> where LessonType : class, ILesson
+    {
+        protected List<LessonType> lessons;
+
+        public LessonDao()
+        {
+            lessons = new List<LessonType>();
+        }
+
+        public bool Add(LessonType lesson)
+        {
+            lesson.Id = lessons.Count > 0 ? lessons.Last().Id + 1 : 1;
+            lessons.Add(lesson);
+            return true;
+        }
+
+        public IEnumerable<LessonType> GetAll()
+        {
+            return lessons.Select(x => x);
+        }
+
+        public LessonType GetById(int id)
+        {
+            return lessons.FirstOrDefault(x => x.Id == id);
+        }
+
+        public bool RemoveById(int id)
+        {
+            LessonType lesson = GetById(id);
+
+            if (lesson is null)
+            {
+                return false;
+            }
+
+            return lessons.Remove(lesson);
+        }
+
+        public bool Update(LessonType lesson)
+        {
+            int index = lessons.FindIndex(x => x.Id == lesson.Id);
+
+            if (index < 0)
+            {
+                return false;
+            }
+
+            lessons[index] = lesson;
+            return true;
+        }
+    }
+}
--- JSCodingStudy/JSCodingStudy.MemoryDAL/Robot/RobotLessonDao.cs
+++ JSCodingStudy/JSCodingStudy.MemoryDAL/Robot/RobotLessonDao.cs
@@ -8,9 +8,14 @@ using System.Threading.Tasks;
 
 namespace JSCodingStudy.MemoryDAL.Robot
 {
-    public class RobotLessonDao : ILessonDao<RobotLessonData>
+    public class RobotLessonDao : LessonDao<RobotLessonData>
     {
-        private List<RobotLessonData> lessons;
+        public RobotLessonDao()
+        {
+            lessons.Add(GenerateLesson1());
+            lessons.Add(GenerateLesson2());
+            lessons.Add(GenerateLesson3());
+        }
 
         private RobotLessonData GenerateLesson1()
         {
@@ -109,56 +114,6 @@ namespace JSCodingStudy.MemoryDAL.Robot
                     Draw = false,
                 },
             };
-        }
-
-        public RobotLessonDao()
-        {
-            lessons = new List<RobotLessonData>();
-            lessons.Add(GenerateLesson1());
-            lessons.Add(GenerateLesson2());
-            lessons.Add(GenerateLesson3());
-        }
-
-        public bool Add(RobotLessonData lesson)
-        {
-            lesson.Id = lessons.Count > 0 ? lessons.Last().Id + 1 : 1;
-            lessons.Add(lesson);
-            return true;
-        }
-
-        public IEnumerable<RobotLessonData> GetAll()
-        {
-            return lessons.Select(x => x);
-        }
-
-        public RobotLessonData GetById(int id)
-        {
-            return lessons.FirstOrDefault(x => x.Id == id);
-        }
-
-        public bool RemoveById(int id)
-        {
-            RobotLessonData lesson = GetById(id);
-
-            if (lesson is null)
-            {
-                return false;
-            }
-
-            return lessons.Remove(lesson);
-        }
-
-        public bool Update(RobotLessonData lesson)
-        {
-            int index = lessons.FindIndex(x => x.Id == lesson.Id);
-
-            if (index < 0)
-            {
-                return false;
-            }
-
-            lessons[index] = lesson;
-            return true;
         }
     }
 }
