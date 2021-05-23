dc69312b78d069d65a2fc1c9099f1ff214199b38
DarkLord <dfhrhfan12@yandex.ru>
2021-05-23 04:02:00 +0400

    Fix fullUser code

--- JSCodingStudy/JSCodingStudy.MemoryDAL/Robot/RobotUserCodeDao.cs
+++ JSCodingStudy/JSCodingStudy.MemoryDAL/Robot/RobotUserCodeDao.cs
@@ -11,21 +11,47 @@ namespace JSCodingStudy.MemoryDAL.Robot
     {
         public RobotUserCodeDao() : base()
         {
-            this[UserDao.FullUser.Id, 1] = "Robot.Move(Directions.Up)\n" +
-                                           "Robot.Move(Directions.Down)\n" +
-                                           "Robot.Move(Directions.Down)";
+            this[UserDao.FullUser.Id, RobotLessonDao.Lesson1.Id] =
+                "Robot.Move(Directions.Up);\n" +
+                "Robot.Move(Directions.Down);\n" +
+                "Robot.Move(Directions.Down);";
 
-            this[UserDao.FullUser.Id, 2] = "Robot.Move(Directions.Left)\n" +
-                                           "Robot.Move(Directions.Right)\n" +
-                                           "Robot.Move(Directions.Right)";
+            this[UserDao.FullUser.Id, RobotLessonDao.Lesson2.Id] =
+                "Robot.Move(Directions.Left);\n" +
+                "Robot.Move(Directions.Right);\n" +
+                "Robot.Move(Directions.Right);";
 
-            this[UserDao.FullUser.Id, 3] = "Robot.Move(Directions.Up)\n" +
-                                           "Robot.Move(Directions.Up)\n" +
-                                           "Robot.Move(Directions.Right)\n" +
-                                           "Robot.Move(Directions.Right)\n" +
-                                           "Robot.Move(Directions.Down)\n" +
-                                           "Robot.Move(Directions.Down)\n" +
-                                           "Robot.Move(Directions.Left)";
+            this[UserDao.FullUser.Id, RobotLessonDao.Lesson3.Id] =
+                "Robot.Move(Directions.Up);\n" +
+                "Robot.Move(Directions.Up);\n" +
+                "Robot.Move(Directions.Right);\n" +
+                "Robot.Move(Directions.Right);\n" +
+                "Robot.Move(Directions.Down);\n" +
+                "Robot.Move(Directions.Down);\n" +
+                "Robot.Move(Directions.Left);";
+
+            this[UserDao.FullUser.Id, RobotLessonDao.Lesson4.Id] =
+                "for(var i = 1; i < 10; i++)\n" +
+                "   Robot.Move(Directions.Right);\n" +
+                "for (var i = 1; i < 5; i++)\n" +
+                "   Robot.Move(Directions.Down);\n" +
+                "for (var i = 1; i < 10; i++)\n" +
+                "   Robot.Move(Directions.Left);\n" +
+                "for (var i = 1; i < 5; i++)\n" +
+                "   Robot.Move(Directions.Up);";
+
+            this[UserDao.FullUser.Id, RobotLessonDao.Lesson5.Id] =
+                "for(var i = 0; i < 3; i++)\n" +
+                "{\n" +
+                "   for (var j = 0; j < 9 - i - i; j++)\n" +
+                "       Robot.Move(Directions.Right);\n" +
+                "   for (var j = 0; j < 4 - i - i; j++)\n" +
+                "       Robot.Move(Directions.Down);\n" +
+                "   for (var j = 0; j < 9 - i - i - 1; j++)\n" +
+                "       Robot.Move(Directions.Left);\n" +
+                "   for (var j = 0; j < 4 - i - i - 1; j++)\n" +
+                "       Robot.Move(Directions.Up);\n" +
+                "}";
         }
     }
 }
--- JSCodingStudy/JSCodingStudy.MemoryDAL/UserCodeDao.cs
+++ JSCodingStudy/JSCodingStudy.MemoryDAL/UserCodeDao.cs
@@ -1,11 +1,6 @@
 ﻿using JSCodingStudy.DALInterfaces;
 using JSCodingStudy.LessonsEntities;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using JSCodingStudy.MemoryDAL.Robot;
 
 namespace JSCodingStudy.MemoryDAL
 {
@@ -16,52 +11,6 @@ namespace JSCodingStudy.MemoryDAL
         public UserCodeDao()
         {
             codes = new Dictionary<int, Dictionary<int, string>>();
-            InitResolvedLessons();
-        }
-
-        private void InitResolvedLessons()
-        {
-            this[UserDao.FullUser.Id, RobotLessonDao.Lesson1.Id] =
-                "Robot.Move(Directions.Up);\n" +
-                "Robot.Move(Directions.Down);\n" +
-                "Robot.Move(Directions.Down);";
-
-            this[UserDao.FullUser.Id, RobotLessonDao.Lesson2.Id] =
-                "Robot.Move(Directions.Left);\n" +
-                "Robot.Move(Directions.Right);\n" +
-                "Robot.Move(Directions.Right);";
-
-            this[UserDao.FullUser.Id, RobotLessonDao.Lesson3.Id] =
-                "Robot.Move(Directions.Up);\n" +
-                "Robot.Move(Directions.Up);\n" +
-                "Robot.Move(Directions.Right);\n" +
-                "Robot.Move(Directions.Right);\n" +
-                "Robot.Move(Directions.Down);\n" +
-                "Robot.Move(Directions.Down);\n" +
-                "Robot.Move(Directions.Left);";
-
-            this[UserDao.FullUser.Id, RobotLessonDao.Lesson4.Id] =
-                "for(var i = 1; i < 10; i++)\n" +
-                "   Robot.Move(Directions.Right);\n" +
-                "for (var i = 1; i < 5; i++)\n" +
-                "   Robot.Move(Directions.Down);\n" +
-                "for (var i = 1; i < 10; i++)\n" +
-                "   Robot.Move(Directions.Left);\n" +
-                "for (var i = 1; i < 5; i++)\n" +
-                "   Robot.Move(Directions.Up);";
-
-            this[UserDao.FullUser.Id, RobotLessonDao.Lesson5.Id] =
-                "for(var i = 0; i < 3; i++)\n" +
-                "{\n" +
-                "   for (var j = 0; j < 9 - i - i; j++)\n" +
-                "       Robot.Move(Directions.Right);\n" +
-                "   for (var j = 0; j < 4 - i - i; j++)\n" +
-                "       Robot.Move(Directions.Down);\n" +
-                "   for (var j = 0; j < 9 - i - i - 1; j++)\n" +
-                "       Robot.Move(Directions.Left);\n" +
-                "   for (var j = 0; j < 4 - i - i - 1; j++)\n" +
-                "       Robot.Move(Directions.Up);\n" +
-                "}";
         }
 
         public bool Add(int user_id, int lesson_id, string code)
